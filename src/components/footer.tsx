cc7168831021bd18e2d6b24a5cd17db2a2a57174
precoder365 <prenitharajesh04@gmail.com>
2023-01-04 20:08:53 +0530

    Responsive style footer elements

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -24,11 +24,8 @@ const IconKnowMore = (props: KnowMoreProps) => {
   return (
     <>
       <a href={props?.link}>
-        <section className="flex items-center justify-start gap-2 py-1 text-lg font-light text-white">
-          <section className="hover:underline">{props?.name}</section>
-          <IoOpenOutline className="hover:translate-x-2">
-            {props?.name}
-          </IoOpenOutline>
+        <section className="flex items-center justify-start gap-2 py-1 text-lg font-thin text-white">
+          <section className="hover:text-xl hover:text-white hover:font-normal text-gray-300">{props?.name}</section>
         </section>
       </a>
     </>
@@ -38,21 +35,21 @@ const IconKnowMore = (props: KnowMoreProps) => {
 const Footer = (): JSX.Element => {
   return (
     <>
-      <footer className="border border-t-[1px] border-b-0 border-r-0 border-l-0 border-textcolors-boundary bg-darkshades-passive py-2 font-Plex">
-        <section className="grid sm:grid-cols-1 md:grid-cols-6 md:gap-2 lg:grid-cols-10">
-          <section className=" hidden items-center justify-start md:col-span-2 md:col-start-1 md:flex lg:col-span-3 lg:col-start-1 lg:flex">
+      <footer className="border border-t-[2px] border-b-0 border-r-0 border-l-0 border-textcolors-boundary bg-darkshades-passive py-2 font-Plex content-center">
+        <section className="p-7 flex flex-row">
+          <section className="md:px-4 lg:pl-7 lg:basis-1/3">
             <StaticImage
               src="../images/banner-logo.png"
               alt="IEEE Computer Society VITC"
-              height={120}
+              height={150}
               layout="fixed"
-              width={275}
-              className="pl-4 md:px-4 "
+              width={350}
+              className="hidden lg:block"
             />
           </section>
-          <section className="mt-4 flex justify-between px-2 sm:col-start-1 sm:py-4 md:col-span-2 md:col-start-3 lg:col-span-2 lg:col-start-4">
-            <section className="text-white">
-              <section className="text-2xl font-extrabold capitalize">
+
+          <section className="basis-1/2 md:basis-1/2 lg:basis-1/3 lg:border lg:border-l-[0.5px] lg:border-r-0 lg:border-t-0 lg:border-b-0 pl-11 sm:pl-1 ml-0">
+              <section className="text-4xl sm:text-2xl mb-2 font-extrabold capitalize text-white flex-nowrap">
                 Know more
               </section>
               <IconKnowMore
@@ -64,23 +61,14 @@ const Footer = (): JSX.Element => {
                 name="Contact us"
               />
               <IconKnowMore link={"https://dev.to/ieeecsvitc"} name={"Blogs"} />
-            </section>
-            <section className="block md:hidden lg:hidden">
-              <StaticImage
-                src="../images/banner-logo.png"
-                alt="IEEE Computer Society VITC"
-                height={78}
-                layout="fixed"
-                width={176}
-                className="px-2 md:px-4"
-              />
-            </section>
+              <IconKnowMore link={"https://dev.to/ieeecsvitc"} name={"Meet the Team"} />
           </section>
-          <section className="mt-4 px-2 sm:col-start-1 sm:flex sm:justify-start sm:py-4 md:col-span-2 md:col-start-5 lg:col-span-2 lg:col-start-8">
-            <section className="text-2xl font-bold text-white">
-              Contact us
-              <section className="flex flex-col justify-start">
-                <IconKnowMore
+
+          <section className="basis-1/2 md:basis-1/2 lg:basis-1/3 pl-11">
+              <section className="text-4xl sm:text-2xl sm:mb-2 mb-2 font-extrabold capitalize text-white">
+                Contact
+              </section>
+              <IconKnowMore
                   name="GitHub"
                   link={"https://github.com/ComputerSocietyVITC"}
                 />
@@ -98,11 +86,10 @@ const Footer = (): JSX.Element => {
                   name="Twitter"
                   link={"https://twitter.com/ieeecsvitc"}
                 />
-              </section>
-            </section>
           </section>
+          
         </section>
-        <section className="sm:text-md col-span-full flex justify-center px-2 font-Monty text-lg font-light text-white">
+        <section className="font-semibold gradient-navbar col-span-full flex justify-center px-2 py-1 font-Monty text-md text-white my-0 sm:text-sm rounded-b-md">
           IEEE Computer Society VIT Chennai © {new Date().getFullYear()}
         </section>
       </footer>
