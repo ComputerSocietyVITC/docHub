e1b64ef6878c8f348447e08f9443b8593f04ee74
0sigma101 <pallav.gupta2021@vitstudent.ac.in>
2023-02-11 20:01:09 +0530

    The final patch is done

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,8 +6,8 @@ const Hero = (): JSX.Element => {
     return (
         <>
         <section className="flex flex-nowrap justify-center font-monty bg-darkshades-passive">
-            <p className="w-3 h-151 text-7xl whitespace-pre-wrap text-textcolors-active leading-84 flex-auto font-bold text-justify py-20 tracking-tighter px-51.7 mx-auto">IEEE Computer Society VIT Chennai's Docs</p>
-            <p className="w-5 h-165.64 font-light text-textcolors-active text-2xl flex-auto text-justify tracking-tight space-y-737 px-51.7 py-51.7 mx-auto">The starting point for your tech journey, Get started and explore you favorite domain in tech. Whether you're just starting out or you're an experienced developer looking to take your skills to the next level, our documentation has something for <strong>everyone</strong>.</p>
+            <p className="w-3 h-151 text-[4.8vw] whitespace-pre-wrap text-textcolors-active leading-84 flex-auto font-bold text-justify py-20 tracking-tighter px-51.7 mx-auto">IEEE Computer Society VIT Chennai's Docs</p>
+            <p className="w-5 h-165.64 font-light text-textcolors-active text-[1.8vw] flex-auto text-justify tracking-tight space-y-737 px-51.7 py-51.7 mx-auto">The starting point for your tech journey, Get started and explore you favorite domain in tech. Whether you're just starting out or you're an experienced developer looking to take your skills to the next level, our documentation has something for <strong>everyone</strong>.</p>
         </section>
     </>
     );
