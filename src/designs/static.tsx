d3bba77c12f72ab85ba726d3da2a52ffe6f5d80c
samshervin <samshervin23@gmail.com>
2023-03-04 22:03:29 +0530

    updated hooks.tsx to open content urls in new tab

--- src/designs/static.tsx
+++ src/designs/static.tsx
@@ -50,10 +50,6 @@ export default TemplateComponent;
 export const Head = ({ pageContext }: any) => {
   return (
     <>
-      <script>
-        document.querySelectorAll("div #contents a").forEach((item) =&gt;
-        item.setAttribute('target', '_blank'))
-      </script>
       <title>{pageContext?.title}</title>
     </>
   );
