7d71597e1ef44e1c476181edf10e465da3adcc4b
samshervin <samshervin23@gmail.com>
2023-03-05 18:50:42 +0530

    done with most search feature stuff

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -1,4 +1,4 @@
-import React, { Attributes, HTMLAttributes, useState } from "react";
+import React, { useState } from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import "../../../css/searchbar.css";
 import { SearchIcon } from "../footer";
