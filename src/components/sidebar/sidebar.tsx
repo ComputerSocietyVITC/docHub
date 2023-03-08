fdb4676e3174ed92e99a8c6f399eb9b3e3b2201e
samshervin <samshervin23@gmail.com>
2023-03-08 11:57:01 +0530

    Sidebar Categorized

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Item from "./item";
+import { element } from "prop-types";
 
 interface ISidebarProps {
   name: string;
@@ -8,18 +9,36 @@ interface ISidebarProps {
 
 const Sidebar = (props: ISidebarProps): JSX.Element => {
   const query = useStaticQuery(graphql`
-    query MarkdownQuery {
+    query {
       allMdx(sort: { frontmatter: { priority: ASC } }) {
         nodes {
           frontmatter {
+            subtopic
             slug
             title
-            subtopic
           }
         }
+        distinct(field: { frontmatter: { subtopic: SELECT } })
       }
     }
   `);
+
+  const data: any[] = query.allMdx.nodes;
+  let subbox = (element:any) => {
+    let insides = (topic:any) => {
+      if (element == topic.frontmatter.subtopic){
+        return(<>
+        {mapMethod(topic)}
+        </>)
+      }
+    }
+    return(<>
+      <section className="text-3xl font-extrabold">{element}</section>
+      <section>{data.map(insides)}</section>
+    </>
+
+    )
+  }
 
   let mapMethod = (element: any) => {
     if (element?.frontmatter.title == props?.name) {
@@ -44,13 +63,15 @@ const Sidebar = (props: ISidebarProps): JSX.Element => {
       );
     }
   };
+  const subtopic_list = query.allMdx.distinct;
 
-  const data: any[] = query.allMdx.nodes;
   return (
     <section className="col-span-2 col-start-1 hidden border border-r-[1px] border-b-0 border-l-0 border-t-0 border-textcolors-boundary bg-darkshades-passive py-2 md:grid lg:grid">
       <aside>
         <nav className="relative z-20 mt-5">
-          <ul>{data.map(mapMethod)}</ul>
+        <div>{subbox('Index')}</div>
+          <div>{subtopic_list.filter((item: string) => ((item!=("Index")) && (item!=("Misc")))).map(subbox)}</div>
+        <div>{subbox('Misc')}</div>
         </nav>
       </aside>
     </section>
